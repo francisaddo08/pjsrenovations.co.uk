45f62400cb38dea78738efe7961abe96fa1058ad
francisaddo08 <45630987+francisaddo08@users.noreply.github.com>
2019-11-16 12:00:45 +0000

    Create README.md

---
+++ wwwroot/js/Modal.js
@@ -0,0 +1,16 @@
+﻿var modal = document.getElementById("galeryModal"); // get modal div
+
+var btn = document.getElementById("modalBtn"); // bet btn
+var closeSpan = document.getElementsByClassName("close")[0] // array of classes
+// now the event handler function
+btn.onclick = function () {
+    modal.style.display = "block";
+
+}
+
+closeSpan.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function () {
+    if (this.event.target == this.modal) { this.modal.style.display = "none"; }
+}
